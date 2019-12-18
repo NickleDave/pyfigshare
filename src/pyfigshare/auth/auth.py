429e114ee4eb5711ef5155a6f49be47d7589f12d
David Nicholson <nickledave@users.noreply.github.com>
2019-12-18 12:23:38 -0500

    remove code from auth.py now in other auth/ modules

--- src/pyfigshare/auth/auth.py
+++ src/pyfigshare/auth/auth.py
@@ -2,117 +2,23 @@
 
 based on ideas from:
 https://github.com/ropensci/rfigshare/blob/master/R/fs_auth.R
-https://github.com/NickleDave/figshare/blob/master/figshare/oauth_dance.py
-https://developer.okta.com/blog/2018/07/16/oauth-2-command-line
-
-some functions/classes modified from
-https://github.com/googleapis/oauth2client/blob/master/oauth2client/tools.py
-https://github.com/googleapis/oauth2client/blob/master/oauth2client/_helpers.py
-under Apache license
-https://github.com/googleapis/oauth2client/blob/master/LICENSE
+https://gist.github.com/burnash/6771295
+https://github.com/rmcgibbo/figshare/blob/master/figshare/oauth_dance.py
+https://developer.okta.com/blog/2018/07/16/oauth-2-command-line/
 """
-from configparser import ConfigParser
-import http.server
 import os
-from pathlib import Path
+
 import ssl
 import sys
-import urllib.parse
+
 import webbrowser
 
 from requests_oauthlib import OAuth2Session
 
-HERE = Path(__file__).parent
-
-# consumer ID and consumer secret are stored in a config.ini file
-CONSUMER_INI = HERE.joinpath('consumer.ini')
-CONFIG = ConfigParser()
-CONFIG.read(CONSUMER_INI)
-# FigShare uses the term 'consumer' ID and secret, instead of 'client'
-CONSUMER_ID = CONFIG['CONSUMER']['ID']
-CONSUMER_SECRET = CONFIG['CONSUMER']['SECRET']
-REDIRECT_URI = CONFIG['CONSUMER']['REDIRECT_URI']
-PORT = CONFIG['CONSUMER']['PORT']
-
-FIGSHARE_AUTHORIZATION_ENDPOINT = 'https://figshare.com/account/applications/authorize'
-FIGSHARE_TOKEN_ENDPOINT = 'https://api.figshare.com/v2/token'
-
-TIMEOUT = 60 * 5  # timeout for SocketServer that waits for request
-
-CERTFILE = './server.pem'
-
-
-# adapted from
-# https://github.com/googleapis/oauth2client/blob/master/oauth2client/_helpers.py
-def parse_unique_urlencoded(content):
-    """Parses unique key-value parameters from urlencoded content.
-
-    Parameters
-    ----------
-    content: str
-        URL-encoded key-value pairs.
-
-    Returns
-    -------
-    params : dict
-    Parameters key-value pairs from ``content``.
-
-    Notes
-    ------
-    Raises ValueError if one of the keys is repeated.
-    """
-    urlencoded_params = urllib.parse.parse_qs(content)
-    params = {}
-    for key, value in urlencoded_params.iteritems():
-        if len(value) != 1:
-            msg = ('URL-encoded content contains a repeated value:'
-                   '%s -> %s' % (key, ', '.join(value)))
-            raise ValueError(msg)
-        params[key] = value[0]
-    return params
-
-
-# adapted from
-# https://github.com/googleapis/oauth2client/blob/master/oauth2client/tools.py
-class RedirectServer(http.server.HTTPServer):
-    """A server to handle OAuth 2.0 redirects back to localhost.
-
-    Waits for a single request and parses the query parameters
-    into query_params and then stops serving.
-    """
-    query_params = {}
-    timeout = TIMEOUT
-
-
-# adapted fromm
-# https://github.com/googleapis/oauth2client/blob/master/oauth2client/tools.py
-class RedirectHandler(http.server.SimpleHTTPRequestHandler):
-    """A handler for OAuth 2.0 redirects back to localhost.
-
-    Waits for a single request and parses the query parameters
-    into the servers query_params and then stops serving.
-    """
-    def __init__(self, request, client_address, server):
-        super().__init__(request, client_address, server)
-        self.params_from_get = None
-
-    def do_GET(self):
-        """Handle a GET request.
-        Parses the query parameters and prints a message
-        if the flow has completed. Note that we can't detect
-        if an error occurred.
-        """
-        self.send_response(http_client.OK)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        parts = urllib.parse.urlparse(self.path)
-        query = parse_unique_urlencoded(parts.query)
-        self.server.query_params = query
-        self.wfile.write(
-            b'<html><head><title>Authentication Status</title></head>')
-        self.wfile.write(
-            b'<body><p>The authentication flow has completed.</p>')
-        self.wfile.write(b'</body></html>')
+from .constants import CONSUMER_ID, CONSUMER_SECRET,
+from .constants import REDIRECT_URI, PORT
+from .constants import FIGSHARE_AUTHORIZATION_ENDPOINT, FIGSHARE_TOKEN_ENDPOINT
+from .oauth import ClientRedirectHandler, ClientRedirectServer
 
 
 def auth(client_id=None,
