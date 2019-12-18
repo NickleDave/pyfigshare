a57b7fdd0274e79467e755147ceb2b5b11fa22d0
David Nicholson <nickledave@users.noreply.github.com>
2019-12-18 12:24:28 -0500

    WIP commit, re-writing oauth.py

--- src/pyfigshare/auth/oauth.py
+++ src/pyfigshare/auth/oauth.py
@@ -1,14 +1,32 @@
-"""tools for OAuth2 from the command line
+# Copyright 2014 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Command-line tools for authenticating via OAuth 2.0
+
+Do the OAuth 2.0 Web Server dance for a command line application. Stores the
+generated credentials in a common file that is used by other example apps in
+the same directory.
 
 modified from
 https://github.com/googleapis/oauth2client/blob/master/oauth2client/tools.py
 https://github.com/googleapis/oauth2client/blob/master/oauth2client/_helpers.py
 under Apache license
 https://github.com/googleapis/oauth2client/blob/master/LICENSE
-
 """
 import http.server
+from http import HTTPStatus
 import urllib.parse
+
 
 TIMEOUT = 60 * 5  # timeout for SocketServer that waits for request
 
@@ -26,7 +44,7 @@ def parse_unique_urlencoded(content):
     Returns
     -------
     params : dict
-    Parameters key-value pairs from ``content``.
+        parameters key-value pairs from ``content``.
 
     Notes
     ------
@@ -45,7 +63,7 @@ def parse_unique_urlencoded(content):
 
 # adapted from
 # https://github.com/googleapis/oauth2client/blob/master/oauth2client/tools.py
-class RedirectServer(http.server.HTTPServer):
+class ClientRedirectServer(http.server.HTTPServer):
     """A server to handle OAuth 2.0 redirects back to localhost.
 
     Waits for a single request and parses the query parameters
@@ -57,23 +75,19 @@ class RedirectServer(http.server.HTTPServer):
 
 # adapted fromm
 # https://github.com/googleapis/oauth2client/blob/master/oauth2client/tools.py
-class RedirectHandler(http.server.SimpleHTTPRequestHandler):
+class ClientRedirectHandler(http.server.BaseHTTPRequestHandler):
     """A handler for OAuth 2.0 redirects back to localhost.
 
     Waits for a single request and parses the query parameters
     into the servers query_params and then stops serving.
     """
-    def __init__(self, request, client_address, server):
-        super().__init__(request, client_address, server)
-        self.params_from_get = None
-
     def do_GET(self):
         """Handle a GET request.
         Parses the query parameters and prints a message
         if the flow has completed. Note that we can't detect
         if an error occurred.
         """
-        self.send_response(http_client.OK)
+        self.send_response(HTTPStatus.OK)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
         parts = urllib.parse.urlparse(self.path)
