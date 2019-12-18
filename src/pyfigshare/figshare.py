93e2b2cea0e8c9b209455c2f9d48b49fb55b6634
David Nicholson <nickledave@users.noreply.github.com>
2019-12-17 23:53:52 -0500

    add pyfigshare/figshare.py module

---
+++ src/pyfigshare/figshare.py
@@ -0,0 +1,18 @@
+"""class for interfacing with Figshare API"""
+
+
+class Figshare:
+    """interface to Figshare API
+
+    Attributes
+    ----------
+
+    Methods
+    -------
+
+    """
+
+    def __init__(self):
+        pass
+
+    def auth(self):
